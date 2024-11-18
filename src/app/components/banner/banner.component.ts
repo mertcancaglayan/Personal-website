2176d48bc37bbe353d154e59fef9e99365c7e267
M.can <caglayan.mertcan37@gmail.com>
2024-11-18 16:11:43 +0300

    add apiService

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { ProfileData, Root } from '../../models/ProfileData.model';
+import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  styleUrls: ['./banner.component.scss'],
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit, OnChanges {
+  data!: Root;
+  profileData!: ProfileData;
+  selectedLanguage: 'english' | 'turkish' = 'english';
 
+  constructor(
+    private apiService: ApiService,
+    private languageService: LanguageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProfileData();
+    this.getLanguage();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedLanguage']) {
+      this.getProfileData(); 
+    }
+  }
+
+  getProfileData(): void {
+    this.apiService.getData().subscribe((res) => {
+      this.data = res;
+      if (this.data && this.data.length > 0) {
+        this.profileData = this.data[0];
+      }
+    });
+  }
+
+  getLanguage(): void {
+    const lang = localStorage.getItem('lang1');
+    this.selectedLanguage = lang === 'turkish' ? 'turkish' : 'english';
+  }
 }
