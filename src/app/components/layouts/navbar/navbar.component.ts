8298b5e43ab60c9ccd5fe60cc0891243d62537af
M.can <caglayan.mertcan37@gmail.com>
2024-11-30 19:36:27 +0300

    add, update cursor

--- src/app/components/layouts/navbar/navbar.component.ts
+++ src/app/components/layouts/navbar/navbar.component.ts
@@ -11,11 +11,6 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  public show = false;
-  public left = '0px';
-  public top = '0px';
-  public scale = 'scale(1)';
-  private firstHover = false;
   isMenuOpen: boolean = false;
 
   constructor(private languageService: LanguageService) {}
@@ -23,31 +18,6 @@ export class NavbarComponent {
   changeLanguage(lang: string): void {
     this.languageService.setLanguage(lang);
     window.location.reload();
-  }
-
-  @ViewChild('navbar') navbarElement!: ElementRef;
-
-  onMouseMove(event: MouseEvent): void {
-    this.show = true;
-    const x = event.clientX;
-    const y = event.clientY;
-
-    if (!this.firstHover) {
-      this.firstHover = true;
-      this.left = `${x}px`;
-      this.top = `${y}px`;
-    } else {
-      this.left = `${x - 10}px`;
-      this.top = `${y - 10}px`;
-    }
-  }
-
-  onMouseOut(): void {
-    this.show = false;
-  }
-
-  onItemHover(isHovering: boolean): void {
-    this.scale = isHovering ? 'scale(2)' : 'scale(1)';
   }
 
   scrollTo(id: string): void {
