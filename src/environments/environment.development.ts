ad89c1728971b03489a7c1c86451913194078632
M.can <caglayan.mertcan37@gmail.com>
2024-12-16 17:41:04 +0300

    update enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  API_URL: 'https://6734ad18a042ab85d11b2e93.mockapi.io/api/v1/personalPage',
+  production: true,
+  API_URL: process.env['API_URL'],
 };
