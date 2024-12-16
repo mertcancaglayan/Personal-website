95f981bfc210222ab2f14b454aa387d8cce04dfb
M.can <caglayan.mertcan37@gmail.com>
2024-12-16 17:29:54 +0300

    add environments"

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ProfileData } from '../models/ProfileData.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = '';
+  private apiUrl = environment.API_URL;
 
   constructor(private http: HttpClient) {}
 
